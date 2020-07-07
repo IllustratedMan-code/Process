25089bb7c13eb85392ba20293147a1efa85eb2b8
IllustratedMan-code <60514384+IllustratedMan-code@users.noreply.github.com>
2020-07-07 12:51:08 -0400

    Update mlink.r

--- R/mlink.r
+++ R/mlink.r
@@ -78,8 +78,17 @@ mlink <- function(metafile, proc = TRUE, filedirectory = FALSE){
     # fdata <- rbind(fdata, c)
   # }
   # }
-  return(data.table::rbindlist(dflist))
-
-
+  activity <- data.table::rbindlist(dflist)
+  activity$region_id <- factor(activity$region_id, levels=unique(activity$region_id))
+  activity$daynumber <- activity$date
+  un <- unique(activity$daynumber)
+  sequence <- seq(length(un)/2)
+  for (i in sequence){
+  activity[activity$daynumber == un[i]]$daynumber <- i
+  activity[activity$daynumber == un[i+12]]$daynumber <- i
+  }
+  activity$daynumber <-as.numeric(activity$daynumber)
+  activity$time <- (activity$daynumber-1) * 60 * 60 * 24 + activity$time
+  return(activity)
 
 }
